8c56f534197d43ed895f34889dfa43e76d561161
Connor Chadwick <cchad2019@gmail.com>
2022-09-12 17:05:53 -0400

    added hw2 files

--- hw1_programming/hw1_submission.py
+++ hw1_programming/hw1_submission.py
@@ -1,6 +1,5 @@
 import collections
 import math
-import numpy as np
 
 ## Note: these are for historical reasons called problem 3a-g but are assigned as problem 1
 ############################################################
@@ -14,7 +13,7 @@ def findAlphabeticallyLastWord(text):
     You might find max() and list comprehensions handy here.
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    return max(text.split())
+    raise Exception("Not implemented yet")
     # END_YOUR_CODE
 
 ############################################################
@@ -26,7 +25,7 @@ def euclideanDistance(loc1, loc2):
     are pairs of numbers (e.g., (3, 5)).
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    return math.sqrt(sum((loc1[i] - loc2[i])**2 for i in [0,1]))
+    raise Exception("Not implemented yet")
     # END_YOUR_CODE
 
 ############################################################
@@ -52,42 +51,13 @@ def mutateSentences(sentence):
                 (reordered versions of this list are allowed)
     """
     # BEGIN_YOUR_CODE (our solution is 21 lines of code, but don't worry if you deviate from this)
-    
-    # using the example code to try to figure out how to do this
-    # create a function inside the function that uses adjacency list to return the length n of the sentences 
-    def adj_search(adj, head, n):
-        if n == 1:
-            return {head} # makes head a set
-        out = set() # makes out a set, not a list
-        for nxt in adj[head]:
-            nxt_sentence = adj_search(adj, nxt, n-1) # finds next setence to compare
-            for ns in nxt_sentence:
-                out.add(head + ' ' + ns)
-        return out
-            
-    # using the new function create the list of similar sentences 
-    word = sentence.split()
-    n = len(word)
-    if n == 0:
-        return
-    # Defaultdict is a sub-class of the dictionary class that returns a dictionary-like object. 
-    # The functionality of both dictionaries and defaultdict are almost same except for the fact 
-    # that defaultdict never raises a KeyError. It provides a default value for the key that does not exists.
-    adj = collections.defaultdict(set)
-    for i in range(n-1):
-        adj[word[i]].add(word[i+1]) # adding the next word to the list
-    if word[-1] not in adj:
-        adj[word[-1]] = set() # takes out the non similar words
-    out = list()
-    for head in adj.keys(): # keys returns a view object
-        out.extend(adj_search(adj, head, n))
-    return out
+    raise Exception("Not implemented yet")
     # END_YOUR_CODE
 
 ############################################################
 # Problem 3d
 
-def sparseVectorDotProduct(v1, v2): 
+def sparseVectorDotProduct(v1, v2):
     """
     Given two sparse vectors |v1| and |v2|, each represented as collections.defaultdict(float), return
     their dot product.
@@ -95,7 +65,7 @@ def sparseVectorDotProduct(v1, v2):
     This function will be useful later for linear classifiers.
     """
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    return sum(v1[i]*v2[i] for i in v1.keys() & v2.keys()) # gets the keys of v1 and v2 and multiplies them. basically dot product calculation
+    raise Exception("Not implemented yet")
     # END_YOUR_CODE
 
 ############################################################
@@ -107,8 +77,7 @@ def incrementSparseVector(v1, scale, v2):
     This function will be useful later for linear classifiers.
     """
     # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
-    for i in v2:
-        v1[i] = v1.get(i,0) + scale * v2[i] # The get() method returns the value of the item with the specified key.
+    raise Exception("Not implemented yet")
     # END_YOUR_CODE
 
 ############################################################
@@ -121,10 +90,7 @@ def findSingletonWords(text):
     You might find it useful to use collections.defaultdict(int).
     """
     # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    word_counter = collections.defaultdict(int)
-    for w in text.split():
-        word_counter[w] +=1
-    return {w for w in word_counter if word_counter[w] == 1}
+    raise Exception("Not implemented yet")
     # END_YOUR_CODE
 
 ############################################################
@@ -156,19 +122,5 @@ def computeLongestPalindromeLength(text):
     up in the increasing order of substring length.
     """
     # BEGIN_YOUR_CODE (our non-recursive solution is 13 lines of code, but don't worry if you deviate from this)
-    def LongestSubLength(i,j):
-        if i == j:
-            return 1
-        elif i > j:
-            return 0
-        elif text[i] == text[j]:
-            return LongestSubLength(i+1, j-1) + 2
-        else:
-            return max(LongestSubLength(i+1, j), LongestSubLength(i, j-1))
-
-    n = len(text)
-    if n <= 1:
-        return n
-    maxlen = LongestSubLength(0, len(text) - 1)
-    return maxlen
+    raise Exception("Not implemented yet")
     # END_YOUR_CODE
