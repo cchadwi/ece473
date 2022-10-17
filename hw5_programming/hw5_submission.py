470dcc5d32923f4bd17d325b1e6fe8d80ab77e67
Connor Chadwick <cchad2019@gmail.com>
2022-10-17 19:17:01 -0400

    added all hw assignments

---
+++ hw5_programming/hw5_submission.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import random
+import collections
+import math
+import sys
+from util import *
+
+############################################################
+# Problem 1: k-means
+############################################################
+
+def kmeans(examples, K, maxIters):
+    '''
+    examples: list of examples, each example is a string-to-double dict representing a sparse vector.
+    K: number of desired clusters. Assume that 0 < K <= |examples|.
+    maxIters: maximum number of iterations to run (you should terminate early if the algorithm converges).
+    Return: (length K list of cluster centroids,
+            list of assignments (i.e. if examples[i] belongs to centers[j], then assignments[i] = j)
+            final reconstruction loss)
+    '''
+    # BEGIN_YOUR_CODE (our solution is 28 lines of code, but don't worry if you deviate from this)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
---
+++ hw9_programming/hw9_submission.py
@@ -0,0 +1,36 @@
+import util, math, random
+from collections import defaultdict
+from util import ValueIteration
+
+############################################################
+# Problem 2a
+
+# If you decide 2a is true, prove it in blackjack.pdf and put "return None" for
+# the code blocks below.  If you decide that 2a is false, construct a counterexample.
+class CounterexampleMDP(util.MDP):
+    # Return a value of any type capturing the start state of the MDP.
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        raise NotImplementedError
+        # END_YOUR_CODE
+
+    # Return a list of strings representing actions possible from |state|.
+    def actions(self, state):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        raise NotImplementedError
+        # END_YOUR_CODE
+
+    # Given a |state| and |action|, return a list of (newState, prob, reward) tuples
+    # corresponding to the states reachable from |state| when taking |action|.
+    # Remember that if |state| is an end state, you should return an empty list [].
+    def succAndProbReward(self, state, action):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        raise NotImplementedError
+        # END_YOUR_CODE
+
+    # Set the discount factor (float or integer) for your counterexample MDP.
+    def discount(self):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        raise NotImplementedError
+        # END_YOUR_CODE
+
